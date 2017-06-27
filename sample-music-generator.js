41ff3e227c06237c98797727358605207e74b20d
himanushi <himanushi@users.noreply.github.com>
2017-06-27 22:05:04 +0900

    コピー機能とか色々追加

--- sample-music-generator.js
+++ sample-music-generator.js
@@ -1,13 +1,14 @@
-;(function() {
+var smg = {}
+;( function() {
 
   'use strict';
 
   var
-    searchUrl   = 'https://itunes.apple.com/search',
-    limit       = 5,
-    offset      = 0,
-    results     = [],
-    results_ele = $( '#results' ),
+    searchUrl       = 'https://itunes.apple.com/search',
+    limit           = 5,
+    offset          = 0,
+    results_ele     = $( '#results' ),
+    affiliate_param = '&at=',
     params = {
       term:      '',
       media:     'music',
@@ -19,6 +20,8 @@
       limit:     limit
     };
 
+  smg.result_list = [];
+
   function searchArtists() {
     $.getJSON(
       searchUrl,
@@ -26,9 +29,8 @@
       function( data, status ) {
 
         $.each( data.results, function( key, result ) {
-          console.dir(result);
           results_ele.append( build_result( result ) );
-          results.push( result );
+          smg.result_list.push( result );
         });
 
         set_params_offset( offset + limit );
@@ -37,7 +39,7 @@
   }
 
   function reset_result() {
-    clear_results();
+    clear_result_list();
     set_params_term( '' );
     reset_params_offset();
     remove_results();
@@ -51,8 +53,8 @@
     results_ele.children().remove();
   }
 
-  function clear_results() {
-    results = [];
+  function clear_result_list() {
+    smg.result_list = [];
   }
 
   function set_params_term( value ) {
@@ -64,25 +66,106 @@
   }
 
   function build_result( result ) {
-    return $('<div>', {
-      'data-song-no': results.length
+    return $( '<div>', {
+      'data-song-no': smg.result_list.length
     }).append(
-      $('<img>', {
+      $( '<img>', {
         src: result.artworkUrl100
       })
     ).append(
-      $('<p>', {
+      $( '<p>', {
         text: result.artistName
       })
     ).append(
-      $('<p>', {
+      $( '<p>', {
         text: result.collectionName
       })
     ).append(
-      $('<p>', {
+      $( '<p>', {
         text: result.trackName
       })
-    )
+    ).click( function( event ) {
+      set_generator( event.currentTarget );
+    })
+  }
+
+  function set_generator( div_ele ) {
+    var
+      generator = $( '#generator' ),
+      preview = $( '#preview' ),
+      result = smg.result_list[ div_ele.dataset.songNo ],
+      sample = sample_generator( result );
+
+    generator.children().remove();
+    generator
+      .append(
+        $( '<textarea>', {
+          id: 'generator_text',
+          text: sample.prop( 'outerHTML' )
+        })
+      );
+
+    preview.children().remove();
+    preview.append( sample );
+
+    $( '#copy' ).css( 'display', 'inline' ).text( 'コピー' );
+  }
+
+  function sample_generator( result ) {
+    return $( '<div>',{
+      class: 'itunes_sample_player'
+    }).append(
+      $( '<img>', {
+        src: result.artworkUrl100
+      })
+    ).append(
+      $( '<div>', {
+        class: 'itunes_sample_info'
+      }).append(
+        $( '<p>', {
+          class: 'itunes_sample_title'
+        }).append(
+          $( '<a>', {
+            href: href_generator( result.trackViewUrl ),
+            target: 'blank',
+            text: result.trackName
+          })
+        )
+      ).append(
+        $( '<p>', {
+          class: 'itunes_sample_artist',
+          text: result.artistName
+        })
+      ).append(
+        $( '<p>', {
+          class: 'itunes_sample_date',
+          text:  date_generator( result.releaseDate )
+        })
+      )
+    ).append(
+      $( '<audio>', {
+        src: result.previewUrl,
+        controls: true
+      })
+    );
+  }
+
+  function href_generator( url ) {
+    var
+      token = $( '#affiliate_token' ).val();
+
+    if ( '' === token ) {
+      return url
+    } else {
+      return url + affiliate_param + token
+    }
+  }
+
+  function date_generator( str_date ) {
+    var
+      d = new Date( str_date );
+
+    return d.getFullYear() + '/' + (d.getMonth() + 1) + '/' + d.getDate()
   }
 
   $( '#term' ).change( function( event ) {
@@ -97,4 +180,11 @@
   $( '#more' ).click( function() {
     searchArtists();
   });
+
+  $( '#copy' ).click( function() {
+    $( '#generator_text' ).select();
+    document.execCommand('copy');
+    $( '#copy' ).text( 'コピーしました！' );
+  });
+
 })();
